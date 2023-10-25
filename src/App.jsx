99dff9bfb027fc3dca42141381aadcd080d886a3
harshahossain <harshahossain@gmail.com>
2023-10-25 21:04:38 +0600

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import "./App.css";
+import mainLogo from "/game-logo.png";
+import Player from "./components/Player";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <header>
+        <img src={mainLogo} alt="Hand Drawn Tic Tac Toe Board" />
+        <h1>Tic-Tac-Toe</h1>
+      </header>
+
+      <div id="game-container">
+        <ol id="players">
+          <Player name="Player-1" symbol="X" />
+          <Player name="Player-2" symbol="O" />
+        </ol>
+        GameBoard
+      </div>
+    </>
+  );
+}
+
+export default App;
