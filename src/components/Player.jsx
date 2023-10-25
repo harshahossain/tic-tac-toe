667c8e665aacfc1626a989d111744ee7ef43c06a
harshahossain <harshahossain@gmail.com>
2023-10-25 21:27:56 +0600

    Update Player.jsx

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,13 +1,21 @@
 import React, { useState } from "react";
 
 export default function Player({ name, symbol }) {
+  //
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleClick() {
+    setIsEditing(!isEditing);
+  }
+
   return (
     <li>
       <span className="player">
-        <span className="player-Name">{name}</span>
+        {!isEditing && <span className="player-Name">{name}</span>}
+        {isEditing && <input type="text"></input>}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={handleClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
